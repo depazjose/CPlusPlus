75e27d6f97c96cc91e3d98b2588e70e068be504f
JOSE DE PAZ <jose.depaz@gmail.com>
2018-12-16 16:47:43 -0500

    Refactoring constants

--- src/constants/constants.hpp
+++ src/constants/constants.hpp
@@ -22,20 +22,19 @@
 #endif
 
 // type of algorithms
-static std::string DEFAULT_ALGORITHM ({"asc"});
+#define DEFAULT_ALGORITHM "asc"
 static std::vector<std::string> ALGORITHMS ({DEFAULT_ALGORITHM,"desc"});
 
-static std::string MSN_INF_ALG_NOT_EXISTS ("INF: Value for sort algorithm is wrong");
-static std::string MSN_INF_ALG ("INF: Will be use default sort algorithm");
+#define MSN_INF_ALG_NOT_EXISTS "INF: Value for sort algorithm is wrong"
+#define MSN_INF_ALG "INF: Will be use default sort algorithm"
 
-static std::string MSN_ERR_INFILE ("ERR: input file wrong or file does not exists");
-static std::string MSN_INF_INFILE ("INF: input file exceeded max lines amount");
-static std::string MSN_INF_INFILE_R ("INF: input file was readed correctly.");
-static std::string MSN_INF_INFILE_P ("INF: Process was terminated prematurely.");
-static std::string MSN_ERR_OUTFILE (
-  "ERR: output file could not be created.\n     check permissions of path");
-static std::string MSN_INF_ITEM_C ("INF: there are items that exceeded max chars amount");
-static std::string MSN_INF_PROC_E ("INF: Process terminated.");
+#define MSN_ERR_INFILE "ERR: input file wrong or file does not exists"
+#define MSN_INF_INFILE "INF: input file exceeded max lines amount"
+#define MSN_INF_INFILE_R "INF: input file was readed correctly"
+#define MSN_INF_INFILE_P "INF: Process was terminated prematurely"
+#define MSN_ERR_OUTFILE "ERR: output file could not be created.\n     check permissions of path"
+#define MSN_INF_ITEM_C "INF: there are items that exceeded max chars amount"
+#define MSN_INF_PROC_E "INF: Process terminated."
 
 
 
